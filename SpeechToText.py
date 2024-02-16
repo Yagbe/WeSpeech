5c0b41a1415fdd9ceefa21c384f162f1b6866fa8
Gkayal2 <123992158+Yagbe@users.noreply.github.com>
2024-02-16 17:20:25 -0500

    Update SpeechToText.py

---
+++ SpeechToText.py
@@ -0,0 +1,32 @@
+import requests
+import time
+
+AUDIO_URL = ""
+API_KEY = ""
+
+HEADERS = {
+    'authorization': API_KEY,
+    'content-type': 'application/json'
+}
+
+URL = ''
+
+res = requests.post(URL,
+    json={'audio_url': AUDIO_URL},
+    headers=HEADERS)
+
+transcript_id = res.json()['id']
+
+while True:
+    polling_endpoint = URL + '/' + transcipt_id
+    res = requests.get(polling_endpoint,
+                        headers=HEADERS)
+    
+    if res.json()['status'] == 'completed':
+        filename = transcript_id + '.txt'
+        with open(filename, 'w') as f:
+            f.write(res.json()['text'])
+
+        break
+    else:
+        time.sleep(60)
